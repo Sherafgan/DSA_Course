d9451861437774960050ea9cf4d8b2cd1ee7f63d
Sherafgan <kssfortis@gmail.com>
2015-11-23 22:43:52 +0300

    simple inefficient implementation of graph & perfect tests class for graph added

--- src/Seminar_10_11_2015_/Graph.java
+++ src/Seminar_10_11_2015_/Graph.java
@@ -1,21 +1,274 @@
 package Seminar_10_11_2015_;
 
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * @author Sherafgan Kandov
- *         23.11.15
+ *         19.11.15
  */
 public class Graph<TDataValue, TWeight> {
-    
+    private List<Vertex> vertices = new ArrayList<>();
+    private List<Edge> edges = new ArrayList<>();
 
-    public Graph() {
+
+    public void insertVertex(TDataValue value) {
+        //Complexity: O(#ofVertices)
+        if (vertexExists(value)) { //O(#ofVertices)
+            throw new IllegalArgumentException("Vertex with value '" + value + "' already exists in graph!");
+        } else {
+            //Complexity: O(1)
+            Vertex vertexToAdd = new Vertex(value, vertices.size());
+            vertices.add(vertexToAdd);
+        }
     }
 
-    class Vertex {
-
+    public void insertEdge(TDataValue origin, TDataValue destination, TWeight weight) {
+        if (!vertexExists(origin)) {
+            throw new NullPointerException("'There is no 'origin' vertex!");
+        } else if (!vertexExists(destination)) {
+            throw new NullPointerException("'There is no 'destination' vertex!");
+        } else {
+            //Complexity: O(1)
+            Edge edgeToAdd = new Edge(origin, destination, weight, edges.size());
+            edges.add(edgeToAdd);
+        }
     }
 
-    class Edge {
+    private boolean vertexExists(TDataValue vertexValue) {
+        //Complexity: O(#ofVertices)
+        for (int i = 0; i < vertices.size(); i++) {
+            if (vertices.get(i).getValue().equals(vertexValue)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void removeVertex(TDataValue value) {
+        //Complexity: O(#ofVertices * #ofEdges)
+        for (int i = 0; i < vertices.size(); i++) {
+            if (vertices.get(i).getValue().equals(value)) {
+                deleteIncidentEdges(i); //O(#ofEdges)
+                vertices.remove(i);
+            }
+        }
+    }
+
+    private void deleteIncidentEdges(int indexOfVertexInTheList) {
+        //Complexity: O(#ofEdges)
+        int i = 0;
+        boolean p = true;
+        while (i < edges.size()) {
+            if (edges.size() > 0
+                    && edges.get(i).getOrigin().equals(vertices.get(indexOfVertexInTheList).getValue())) {
+                edges.remove(i);
+                p = false;
+            }
+            if (edges.size() > 0
+                    && edges.get(i).getDestination().equals(vertices.get(indexOfVertexInTheList).getValue())) {
+                edges.remove(i);
+                p = false;
+            }
+            if (p) {
+                i++;
+            } else {
+                p = true;
+            }
+        }
+    }
+
+    public void removeEdge(TDataValue origin, TDataValue destination, TWeight weight) {
+        //Complexity: O(#ofEdges)
+        for (int i = 0; i < edges.size(); i++) {
+            if (edges.get(i).getWeight().equals(weight)
+                    && edges.get(i).getOrigin().equals(origin)
+                    && edges.get(i).getDestination().equals(destination)) {
+                edges.remove(i);
+            }
+        }
+    }
+
+    public void removeAllEdgesWithWeight(TWeight weight) {
+        //Complexity: O(#ofEdges)
+        for (int i = 0; i < edges.size(); i++) {
+            if (edges.get(i).getWeight().equals(weight)) {
+                edges.remove(i);
+            }
+        }
+    }
+
+    public List<TWeight> incidentEdges(TDataValue vertexValue) {
+        //Complexity: O(#ofEdges)
+        List<TWeight> incidentEdges = new ArrayList<>();
+        for (int i = 0; i < edges.size(); i++) {
+            if (edges.get(i).getOrigin().equals(vertexValue)
+                    || edges.get(i).getDestination().equals(vertexValue)) {
+                incidentEdges.add(edges.get(i).getWeight());
+            }
+        }
+        if (incidentEdges.size() == 0) {
+            return null;
+        } else {
+            return incidentEdges;
+        }
+    }
+
+    public List<TDataValue> endVertices(TWeight weight) {
+        //Complexity: O(#ofEdges)
+        List<TDataValue> endVertices = new ArrayList<>();
+        for (int i = 0; i < edges.size(); i++) {
+            if (edges.get(i).getWeight().equals(weight)) {
+                endVertices.add(edges.get(i).getOrigin());
+                endVertices.add(edges.get(i).getDestination());
+                return endVertices;
+            }
+        }
+        return null;
+    }
+
+    public TDataValue opposite(TDataValue vertexValue, TWeight weight) {
+        //Complexity: O(#ofEdges)
+        for (int i = 0; i < edges.size(); i++) {
+            if (edges.get(i).getOrigin().equals(vertexValue) && edges.get(i).getWeight().equals(weight)) {
+                return edges.get(i).getDestination();
+            } else if (edges.get(i).getDestination().equals(vertexValue) && edges.get(i).getWeight().equals(weight)) {
+                return edges.get(i).getOrigin();
+            }
+        }
+        return null;
+    }
+
+    public boolean areAdjacent(TDataValue firstVertexValue, TDataValue secondVertexValue) {
+        //Complexity: O(#ofEdges)
+        for (int i = 0; i < edges.size(); i++) {
+            if ((edges.get(i).getOrigin().equals(firstVertexValue)
+                    && edges.get(i).getDestination().equals(secondVertexValue))
+                    || (edges.get(i).getDestination().equals(firstVertexValue)
+                    && edges.get(i).getOrigin().equals(secondVertexValue))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int degree(TDataValue vertexValue) {
+        //Complexity: O(#ofEdges)
+        //implementation works only for ACYCLIC GRAPH
+        int degree = 0;
+        for (int i = 0; i < edges.size(); i++) {
+            if (edges.get(i).getOrigin().equals(vertexValue)
+                    || edges.get(i).getDestination().equals(vertexValue)) {
+                degree++;
+            }
+        }
+        return degree;
+    }
+
+    public int amountOfVertices() {
+        return vertices.size();
+    }
+
+    public int amountOfEdges() {
+        return edges.size();
+    }
+
+    private class Vertex {
+        private TDataValue value;
+        private int listPosition;
+        private int adjacencyListPosition;
+
+        public Vertex(TDataValue value, int listPosition) {
+            this.value = value;
+            this.listPosition = listPosition;
+        }
+
+        public TDataValue getValue() {
+            return value;
+        }
+
+        public void setValue(TDataValue value) {
+            this.value = value;
+        }
+
+        public int getListPosition() {
+            return listPosition;
+        }
+
+        public void setListPosition(int listPosition) {
+            this.listPosition = listPosition;
+        }
+
+        public int getAdjacencyListPosition() {
+            return adjacencyListPosition;
+        }
+
+        public void setAdjacencyListPosition(int adjacencyListPosition) {
+            this.adjacencyListPosition = adjacencyListPosition;
+        }
+    }
+
+    private class Edge {
+        private TDataValue origin;
+        private TDataValue destination;
+        private TWeight weight;
+        private int listPosition;
+
+        private int adjacencyListPosition1;
+        private int getAdjacencyListPosition2;
+
+        public Edge(TDataValue origin, TDataValue destination, TWeight weight, int listPosition) {
+            this.origin = origin;
+            this.destination = destination;
+            this.weight = weight;
+            this.listPosition = listPosition;
+        }
+
+        public TDataValue getOrigin() {
+            return origin;
+        }
+
+        public void setOrigin(TDataValue origin) {
+            this.origin = origin;
+        }
+
+        public TDataValue getDestination() {
+            return destination;
+        }
+
+        public void setDestination(TDataValue destination) {
+            this.destination = destination;
+        }
+
+        public TWeight getWeight() {
+            return weight;
+        }
+
+        public void setWeight(TWeight weight) {
+            this.weight = weight;
+        }
+
+        public int getListPosition() {
+            return listPosition;
+        }
+
+        public void setListPosition(int listPosition) {
+            this.listPosition = listPosition;
+        }
+
+        public int getAdjacencyListPosition1() {
+            return adjacencyListPosition1;
+        }
+
+        public void setAdjacencyListPosition1(int adjacencyListPosition1) {
+            this.adjacencyListPosition1 = adjacencyListPosition1;
+        }
+
+        public int getAdjacencyListPosition2() {
+            return getAdjacencyListPosition2;
+        }
+
+        public void setGetAdjacencyListPosition2(int getAdjacencyListPosition2) {
+            this.getAdjacencyListPosition2 = getAdjacencyListPosition2;
+        }
     }
 }
--- src/Seminar_10_11_2015_/tests/GraphTests.java
+++ src/Seminar_10_11_2015_/tests/GraphTests.java
@@ -1,6 +1,6 @@
 package Seminar_10_11_2015_.tests;
 
-import Seminar_10_11_2015_.OldGraph;
+import Seminar_10_11_2015_.Graph;
 import junit.framework.TestCase;
 import org.junit.Test;
 
@@ -11,15 +11,154 @@ import org.junit.Test;
 public class GraphTests extends TestCase {
     @Test
     public void test() {
-        OldGraph<String, String> oldGraph = new OldGraph<>();
+        //All the tests in this method are done to the following graph
+        //          v
+        //         / \
+        //        /   \
+        //     a /     \ b
+        //      /       \
+        //     /    c    \
+        //    u-----------w
 
-        oldGraph.insertVertex("u");
-        oldGraph.insertVertex("v");
-        oldGraph.insertVertex("w");
+        Graph<String, String> graph = new Graph<>();
 
-        assertTrue(oldGraph.amountOfVertices() == 3);
-        assertTrue(oldGraph.amountOfEdges() == 0);
+        graph.insertVertex("u");
+        graph.insertVertex("v");
+        graph.insertVertex("w");
 
-        oldGraph.insertEdge("u", "v", "a");
+        assertTrue(graph.amountOfVertices() == 3);
+        assertTrue(graph.amountOfEdges() == 0);
+
+        graph.insertEdge("u", "v", "a");
+        graph.insertEdge("v", "w", "b");
+
+        assertTrue(graph.amountOfEdges() == 2);
+
+        graph.removeVertex("v");
+
+        assertTrue(graph.amountOfEdges() == 0);
+        assertTrue(graph.amountOfVertices() == 2);
+
+        graph.insertVertex("v");
+        graph.insertEdge("u", "v", "a");
+        graph.insertEdge("v", "w", "b");
+        graph.insertEdge("u", "w", "c");
+
+        assertTrue(graph.amountOfVertices() == 3);
+        assertTrue(graph.amountOfEdges() == 3);
+
+        assertSame(graph.degree("u"), 2);
+        assertSame(graph.degree("v"), 2);
+
+        assertEquals(graph.endVertices("a").get(0), "u");
+        assertEquals(graph.endVertices("a").get(1), "v");
+
+        assertEquals(graph.opposite("u", "a"), "v");
+
+        assertEquals(graph.incidentEdges("v").get(0), "a");
+        assertEquals(graph.incidentEdges("v").get(1), "b");
+
+        graph.removeAllEdgesWithWeight("a");
+
+        assertTrue(graph.amountOfEdges() == 2);
+        assertFalse(graph.areAdjacent("u", "v"));
+        assertTrue(graph.areAdjacent("u", "w"));
+
+        assertSame(graph.degree("u"), 1);
+        assertSame(graph.degree("v"), 1);
+
+        assertEquals(graph.endVertices("b").get(0), "v");
+        assertEquals(graph.endVertices("b").get(1), "w");
+
+        assertEquals(graph.opposite("v", "b"), "w");
+        assertEquals(graph.opposite("w", "b"), "v");
+
+        assertEquals(graph.incidentEdges("v").get(0), "b");
+    }
+
+    @Test
+    public void test2() {
+        //Tests for another more complex undirected weighted graph
+        //Link to image of graph: http://i.stack.imgur.com/kJF5K.png
+
+        Graph<String, Integer> graph = new Graph<>();
+
+        graph.insertVertex("A");
+        graph.insertVertex("B");
+        graph.insertVertex("C");
+        graph.insertVertex("D");
+        graph.insertVertex("E");
+        graph.insertVertex("F");
+        graph.insertVertex("G");
+        graph.insertVertex("H");
+        graph.insertVertex("I");
+
+        assertSame(graph.amountOfVertices(), 9);
+
+        graph.insertEdge("A", "B", 22);
+        graph.insertEdge("A", "C", 9);
+        graph.insertEdge("A", "D", 12);
+
+        graph.insertEdge("B", "C", 35);
+        graph.insertEdge("B", "F", 36);
+        graph.insertEdge("B", "H", 34);
+
+        graph.insertEdge("C", "D", 4);
+        graph.insertEdge("C", "E", 65);
+        graph.insertEdge("C", "F", 42);
+
+        graph.insertEdge("D", "E", 33);
+        graph.insertEdge("D", "I", 30);
+
+        graph.insertEdge("E", "F", 18);
+        graph.insertEdge("E", "G", 23);
+
+        graph.insertEdge("F", "G", 39);
+        graph.insertEdge("F", "H", 24);
+
+        graph.insertEdge("G", "H", 25);
+        graph.insertEdge("G", "I", 21);
+
+        graph.insertEdge("H", "I", 19);
+
+        assertSame(graph.amountOfEdges(), 18);
+
+        graph.removeEdge("A", "B", 22);
+        assertSame(graph.amountOfEdges(), 17);
+
+        assertSame(graph.opposite("B", 34), "H");
+
+        assertSame(graph.incidentEdges("E").get(0), 65);
+        assertSame(graph.incidentEdges("E").get(1), 33);
+        assertSame(graph.incidentEdges("E").get(2), 18);
+        assertSame(graph.incidentEdges("E").get(3), 23);
+
+        assertSame(graph.endVertices(25).get(0), "G");
+        assertSame(graph.endVertices(25).get(1), "H");
+
+        assertSame(graph.endVertices(65).get(0), "C");
+        assertSame(graph.endVertices(65).get(1), "E");
+
+        assertTrue(graph.areAdjacent("D", "I"));
+        assertTrue(graph.areAdjacent("E", "F"));
+        assertTrue(graph.areAdjacent("C", "D"));
+        assertTrue(graph.areAdjacent("I", "H"));
+
+        assertFalse(graph.areAdjacent("A", "F"));
+        assertFalse(graph.areAdjacent("C", "I"));
+
+        assertSame(graph.degree("E"), 4);
+        assertSame(graph.degree("A"), 2); // A-B was removed
+        assertSame(graph.degree("I"), 3);
+        assertSame(graph.degree("F"), 5);
+
+        assertSame(graph.opposite("C", 9), "A");
+        assertSame(graph.opposite("E", 18), "F");
+        assertSame(graph.opposite("G", 39), "F");
+        assertSame(graph.opposite("E", 18), "F");
+        assertSame(graph.opposite("I", 19), "H");
+
+        graph.removeVertex("F");
+        assertSame(graph.amountOfEdges(), 12);
     }
 }
